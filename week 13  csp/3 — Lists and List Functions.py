d6345a840f01f9a6dbf4ac24061c9fbf3d940086
Jy654 <JNavarro69@cps.edu>
2025-11-14 15:34:11 +0000

    lists

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -54,31 +54,87 @@ print(len(my_list))
 # .remove() - removes thje f9irst occurence of a specified value 
 # .sort() - sorts the list in ascending order 
 # .reverse() - reverses the order of the list
-# my_list = ['apple', 'banana', 'cherry']
-# print(my_list[0])         # apple
-# print(my_list[1:])        # ['banana', 'cherry']
+###################################################
+# why is a list more usedul trhan a variable ?
+# A list can hold mutiple values,
+# While a variable can only hold one value at a time
+cakes = ['chocolate', 'vanilla', 'red velvet', 'carrot']
+print(cakes)
 
-# my_list.append('grape')
-# print(my_list)
+print(cakes[0])
 
-# my_list.pop(1)
-# print(my_list)
+print(cakes[-1])
 
-# numbers = [3, 1, 4, 2]
-# numbers.sort()
-# print(numbers)
+cakes[0] = 'strawberry'
+print(cakes)
+
+cakes.append('lemon')
+print(cakes)
+
+cakes[1] = 'chocolate'
+print(cakes)
+
+cakes.pop()
+print(cakes)
+
+cakes.insert(2, 'funfetti')
+print(cakes)
+
+
+my_list = ['apple', 'banana', 'cherry']
+print(my_list[0])         # apple
+print(my_list[1:])        # ['banana', 'cherry']
+
+my_list.append('grape')
+print(my_list)
+
+my_list.pop(1)
+print(my_list)
+
+numbers = [3, 1, 4, 2]
+numbers.sort()
+print(numbers)
 
 
 # # Practice Problems:
 
 # # Create a list with 5 of your favorite foods.
-
+food = ['pizza', 'wings', 'empanadas', 'tacos', 'fried chicken']
+print(food)
 # # Print the second and last item.
-
+print(food[-4])
+print(food[-1])
 # # Add a new item using .append().
-
+food.append('cheesecake')
+print(food)
 # # Remove the first item using .pop(0).
-
+food.pop(0)
+print(food)
 # # Reverse your list using .reverse().
+food.reverse()
+print(food)
 
+# sets= {1, 2, 3}
+# sets aren unordered collections of unique items
+# sets do not support indexing or slicing
+# sets are mutable, meaning you can add or remove items
+# sets arew created using curly braces {}
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+print(type(my_set))
+my_set.add(6)
+print(my_set)
+my_set.remove(3)
+print(my_set)
+print(4 in my_set)
+print(3 in my_set)
+
+# tuples arew ordered collections of items 
+# tuples are immutable, meaning you cannot modify them after creation
+# tuples are created using parentheses ()
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+print(type(my_tuple))
+print(my_tuple[0])
+print(my_tuple[1:4])
 # # Create a list of 3 lists (matrix), and access the middle element.
